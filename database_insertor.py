d2ecaba561c990e900bc4ef3c1e1a38c393b4ad3
ASH <ash@kali>
2023-08-23 14:01:17 +0330

    writing a database insertor code

---
+++ database_insertor.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+# Connect to the SQLite database
+db_connection = sqlite3.connect('app_data.db')
+db_cursor = db_connection.cursor()
+
+# Read the questions from the file
+with open('questions.txt', 'r') as questions_file:
+    questions_data = questions_file.read()
+
+# Read the answers from the file
+with open('answers.txt', 'r') as answers_file:
+    answers_data = answers_file.read()
+
+# Split the text into individual questions and answers
+questions = questions_data.split('Question ')
+answers = answers_data.split('Answer ')
+
+# Iterate through the questions and answers and insert them into the database
+for question, answer in zip(questions[1:], answers[1:]):
+    question_lines = question.strip().split('\n')
+    question_text = question_lines[1].strip()
+    options = [option.strip() for option in question_lines[2:] if option.strip()]  # Extract options
+    
+    answer_lines = answer.strip().split('\n')
+    answer_text = answer_lines[0].split(": ")[1] if len(answer_lines) > 0 else "(No answer provided)"
+
+    # Convert options list to a formatted string
+    options_text = "\n".join(options)
+    
+    db_cursor.execute(
+        "INSERT INTO qa_table (question, options, answer, grade) VALUES (?, ?, ?, ?)",
+        (question_text, options_text, answer_text, "A1-A2")
+    )
+
+# Commit the changes and close the database connection
+db_connection.commit()
+db_connection.close()
+
+print("Data inserted successfully.")
