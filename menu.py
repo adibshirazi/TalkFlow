1be7f57665511e6bc629756ade5f6c2d0991152c
ASH <mgmt@byash.xyz>
2023-08-23 20:30:31 +0330

    Updated to ask for the User's name and store it in the DB

--- menu.py
+++ menu.py
@@ -1,4 +1,20 @@
 # menu.py
+import sqlite3
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+def username():
+    cursor.execute("SELECT * FROM user")
+    user_record = cursor.fetchone()
+    
+    if user_record is None:
+        name = input("What's Your Name? ")
+        cursor.execute("INSERT INTO user (name) VALUES (?)", (name,))
+        conn.commit()
+        return name
+    else:
+        return user_record[0]  # Assuming name is the first column
 
 def display_menu():
     print("1. Start Quiz")
@@ -12,3 +28,4 @@ def get_user_choice():
         print("Invalid choice. Please select again.")
         choice = display_menu()
     return choice
+ 
