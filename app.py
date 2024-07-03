c22ba7f3b7627e3e87ad6e19271f6103bff6e608
adibshirazi <adibarabishirazi83@gmail.com>
2024-07-03 12:49:18 +0330

    add some modification

--- app.py
+++ app.py
@@ -21,50 +21,62 @@ def ask_question(question_data, question_number):
     return user_input.upper()
 
 
+
+def grades():
+    user_choice = menu.get_user_choice()
+
+    if user_choice == '1':
+        grade = str(input("Enter your grade (a1-a2, b1-b2, etc.): ").upper())
+        return grade
+
+
 def main():
     print("Welcome to the English Quiz!")
     name = menu.username()  # Get the user's name
     print(f"Hello, {name}!")
 
-    user_choice = menu.get_user_choice()
+    
+    questions_to_ask = 5
+    total_correct = 0
+    levels = ["A1-A2", "B1-B2"]
 
-    if user_choice == '1':
-        grade = input("Enter your grade (a1-a2, b1-b2, etc.): ").upper()
-        questions_to_ask = 5
-        total_correct = 0
+    while True:
+        grade = grades()
+        if grade not in levels:
+            print("Wrong grade. Please try again!")
+            pass
+        else:
+            break
+    questions = fetch_questions(grade)
+   
+    if grade in levels and len(questions) < questions_to_ask:
+        print("Not enough questions for your grade level.")
+        return
 
-        questions = fetch_questions(grade)
+    random.shuffle(questions)
+    asked_questions = set()
 
-        if len(questions) < questions_to_ask:
-            print("Not enough questions for your grade level.")
-            return
-
-        random.shuffle(questions)
-        asked_questions = set()
-
-        for question_number, question_data in enumerate(questions, start=1):
-            while True:
-                if question_data in asked_questions:
-                    question_data = random.choice(questions)
-                else:
-                    asked_questions.add(question_data)
-                    break
-
-            user_answer = ask_question(question_data, question_number)
-            correct_answer = question_data[2]
-            if user_answer == correct_answer[0]:
-                total_correct += 1
-                print("Correct!\n")
+    for question_number, question_data in enumerate(questions, start=1):
+        while True:
+            if question_data in asked_questions:
+                question_data = random.choice(questions)
             else:
-                print(f"Wrong. The correct answer is: {correct_answer}\n")
-
-            if question_number == questions_to_ask:
+                asked_questions.add(question_data)
                 break
 
-        score = (total_correct / questions_to_ask) * 10
-        print(f"Quiz completed! Your score: {score:.2f}/10")
-    else:
-        print("\nGoodbye!")
+        user_answer = ask_question(question_data, question_number)
+        correct_answer = question_data[2]
+        if user_answer == correct_answer[0]:
+            total_correct += 1
+            print("Correct!\n")
+        else:
+            print(f"Wrong. The correct answer is: {correct_answer}\n")
+
+        if question_number == questions_to_ask:
+            break
+
+    score = (total_correct / questions_to_ask) * 10
+    print(f"Quiz completed! Your score: {score:.2f}/10")
 
     conn.close()
 
